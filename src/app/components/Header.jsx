88e120b2f98e6736b1d3b60311175ebc0eead7f6
Sakib Hossain <sakibs.ngn@gmail.com>
2024-08-23 04:12:36 +0600

    2nd

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -10,22 +10,19 @@ const Header = () => {
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I'm {""}</span>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I`m {""}</span>
                         <br></br>
                         <TypeAnimation
                             sequence={[
-                                // Same substring at the start will only be typed out once, initially
                                 'Sakib Hossain',
-                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                1000,
                                 'Web Developer',
                                 1000,
                                 'Front End Web Developer',
                                 1000,
-
                             ]}
                             wrapper="span"
                             speed={50}
-
                             repeat={Infinity}
                         />
                     </h1>
