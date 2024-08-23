ba15e70c48b9c266caf426fb990b7ef8b53191ff
Sakib Hossain <sakibs.ngn@gmail.com>
2024-08-24 03:34:14 +0600

    animati22on

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_DJt9XC62_FPTcarFHF2R9XsLFfuUsgCym');
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
